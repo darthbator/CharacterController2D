6b5e16b94e20adb7c11989c00115cc4530a606fd
David Bator <dbator@3blackdot.com>
2016-10-25 16:26:52 -0700

    Initial refactor commenting out slope ascent/descent, one way platforms, and gravity

---
+++ Assets/demo/scripts/DemoControls.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DemoControls : MonoBehaviour {
+	public float moveSpeed;
+	private Vector2 moveAmount;
+	private OverheadCharacterController2D characterController;
+
+	void Start () {
+		characterController = GetComponent<OverheadCharacterController2D>();
+	}
+
+	void Update () {
+		moveAmount = Vector2.zero;
+
+		if (Input.GetKey(KeyCode.W))
+			moveAmount.y += 1f * moveSpeed * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.S))
+			moveAmount.y += -1f * moveSpeed * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.A))
+			moveAmount.x += -1f * moveSpeed * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.D))
+			moveAmount.x += 1f * moveSpeed * Time.deltaTime;
+
+		if (moveAmount != Vector2.zero)
+			characterController.move(moveAmount);
+	}
+}
